b340c6680cfb39a8774d902542c4b6a2ee032d19
maynul1997 <mamun.harf@gmail.com>
2021-02-07 11:25:17 +0600

    null vs undefined cleared for interview

---
+++ null-undefined.js
@@ -0,0 +1,20 @@
+
+//let pakhi;
+//console.log(pakhi);
+
+// function add(num1,num2){
+//     console.log(num1, num2);
+// }
+
+// function add(num1, num2){
+//     console.log(num1+num2);
+// }
+
+// const premik ={name: "smart", phone: 09876};
+// console.log(premik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [2, 6, 8];
+console.log(ages[10]);
