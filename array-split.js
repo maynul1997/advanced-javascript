c5b6976aa6a510b7b5fc2e4fab7b130e240c7ec4
maynul1997 <mamun.harf@gmail.com>
2021-02-08 13:10:09 +0600

    split, splice done

---
+++ array-split.js
@@ -0,0 +1,11 @@
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+//const part = nums.slice(2,5);
+const remove = nums.splice(1,6,77); // amazing part
+
+// console.log(remove);
+// console.log(nums);
+
+const together = nums.join(" ");
+//const together = nums.join("");
+console.log(together);
